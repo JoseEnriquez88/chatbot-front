d19a746a389e7bd87c1ceb4428dcf720774ab68d
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-27 17:18:05 -0300

    added footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,34 @@
+"use client";
+import { MdArrowOutward } from "react-icons/md";
+import logo from "@/assets/logo.webp";
+import Image from "next/image";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-transparent border border-solid border-gray-800 w-full h-[100px] pt-[24px] px-[8px] pb-[56px] flex flex-col items-center gap-1">
+      <div className="flex items-center gap-1">
+        <Image src={logo} alt="logo" loading="lazy" className="size-4" />
+        <a
+          href="https://www.promtior.ai/"
+          target="_blank"
+          rel="noreferrer"
+          className="font-pc-bebas text-pc-white text-lg cursor-pointer"
+        >
+          promtior
+        </a>
+      </div>
+      <p className="text-pc-white font-pc-lato text-xs md:text-sm leading-[20px] tracking-normal flex font-bold">
+        Desarrollado por
+        <a
+          href="https://enriquez-jose.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="underline ml-1 flex items-center gap-1 transition-colors duration-300 ease-in-out cursor-pointer"
+        >
+          Jose Enriquez
+          <MdArrowOutward />
+        </a>
+      </p>
+    </footer>
+  );
 };
 export default Footer;
