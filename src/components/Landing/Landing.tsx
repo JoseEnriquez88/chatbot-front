284fc38b2fa4adc19d885be78dee3934c294c32c
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-27 17:18:14 -0300

    updated landing component

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -2,10 +2,11 @@
 import logo from "@/assets/logo.webp";
 import Image from "next/image";
 import LandingButton from "./LandingButton/LandingButton";
+import Footer from "../Footer/Footer";
 
 const Landing = () => {
   return (
-    <div className="h-screen overflow-hidden relative gradient-background">
+    <div className="h-screen flex flex-col justify-between gradient-background">
       <div className="flex flex-col items-center justify-center pt-[100px] px-[20px] md:pt-[200px] gap-6 text-pc-white">
         <Image src={logo} alt="Promtior" loading="lazy" className="size-16" />
         <h1 className="font-pc-bebas text-4xl md:text-5xl uppercase">
@@ -16,7 +17,9 @@ const Landing = () => {
         </p>
         <LandingButton />
       </div>
+      <Footer /> {/* Se mantiene abajo sin expandirse */}
     </div>
   );
 };
 export default Landing;
+
