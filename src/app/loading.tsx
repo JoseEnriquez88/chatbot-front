59993b4d1a34cefa0f3eaaafe715e25f1077a6c4
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-27 23:36:51 -0300

    added loader component

---
+++ src/app/loading.tsx
@@ -0,0 +1,19 @@
+"use client";
+import logo from "@/assets/logo.webp";
+import Image from "next/image";
+const loading = () => {
+  return (
+    <div className="bg-pc-violet h-screen w-screen flex flex-col items-center justify-center gap-4">
+      <Image
+        src={logo}
+        alt="logo"
+        loading="lazy"
+        className="size-30 animate-spin"
+      />
+      <h1 className="font-pc-bebas font-bold text-pc-white text-4xl animate-pulse">
+        cargando
+      </h1>
+    </div>
+  );
+};
+export default loading;
