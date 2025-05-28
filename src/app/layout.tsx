73e82e70ee83f5d06ab8f0c8a637e1dadf7c1b82
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-28 20:52:20 -0300

    layout updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className="antialiased gradient-background">
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
