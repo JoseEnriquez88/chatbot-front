320d0e52a705a765565335dd262ded5d39fdad21
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-27 23:37:40 -0300

    added loader component in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Landing from "@/components/Landing/Landing";
-export default function Home() {
+export default async function Home() {
+  await new Promise((r) => setTimeout(r, 3000));
   return <Landing />;
 }
