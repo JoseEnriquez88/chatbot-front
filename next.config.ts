15d98d7eaf05f175a2661f0c5621e4b43946f9e2
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-28 02:54:58 -0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/chat",
+        destination: "http://18.118.162.255:8000/chat",
+      },
+    ];
+  },
 };
-
-export default nextConfig;
+module.exports = nextConfig;
