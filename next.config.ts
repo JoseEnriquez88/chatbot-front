81186f5f47d8d933daa2ea978dc9f0646ce5eda5
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-28 03:11:27 -0300

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js (en la raíz, commiteado y versionado)
+module.exports = {
   async rewrites() {
     return [
       {
-        source: "/api/chat",
-        destination: "http://18.118.162.255:8000/chat",
+        source: '/api/:path*',             // todas las llamadas que empiecen con /api/
+        destination: 'http://18.118.162.255:8000/:path*',  
       },
     ];
   },
-};
-module.exports = nextConfig;
+}
