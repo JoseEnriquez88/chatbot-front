f804665cbd868891f41f8baa227d75114195c10e
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-27 23:38:28 -0300

    updated message bubble component

--- src/components/Chatbot/MessageBubble.tsx
+++ src/components/Chatbot/MessageBubble.tsx
@@ -4,13 +4,19 @@ export default function MessageBubble({ role, text }: MessageBubbleProps) {
   const isUser = role === "user";
 
   return (
-    <div className={`flex w-full font-bold ${isUser ? "justify-end" : "justify-start"}`}>
+    <div
+      className={`flex w-full font-bold ${
+        isUser ? "justify-end" : "justify-start"
+      }`}
+    >
       <div
         className={`
-          p-4 whitespace-pre-wrap rounded-2xl shadow-md
-          ${isUser
-            ? "bg-pc-violet-100 text-pc-white w-auto max-w-[75%]"
-            : "bg-pc-violet text-pc-white w-full"}
+          p-4 whitespace-pre-wrap rounded-2xl 
+          ${
+            isUser
+              ? "bg-pc-violet-100 text-pc-white w-auto max-w-[75%]"
+              : "text-pc-white w-full"
+          }
         `}
       >
         {text}
@@ -18,4 +24,3 @@ export default function MessageBubble({ role, text }: MessageBubbleProps) {
     </div>
   );
 }
-
