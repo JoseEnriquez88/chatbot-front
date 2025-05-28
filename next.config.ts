30f19f83a38583750738ecf65b54e8e245eaae10
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-28 03:26:21 -0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-// next.config.js (en la raíz, commiteado y versionado)
 module.exports = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',             // todas las llamadas que empiecen con /api/
-        destination: 'http://18.118.162.255:8000/:path*',  
+        source: '/api/:path*',
+        destination: 'http://18.118.162.255:8000/:path*',
       },
     ];
   },
-}
+};
