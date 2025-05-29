077cffbee88f810d8a95275a3368624ab3dd5166
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-29 07:39:36 -0300

    updated landing button component

--- src/components/Landing/LandingButton/LandingButton.tsx
+++ src/components/Landing/LandingButton/LandingButton.tsx
@@ -2,15 +2,7 @@
 import { useRouter } from "next/navigation";
 import { MdOutlineArrowOutward } from "react-icons/md";
 import { motion } from "framer-motion";
-
-const buttonVariants = {
-  hidden: { opacity: 0, scale: 0.8 },
-  show: {
-    opacity: 1,
-    scale: 1,
-    transition: { type: "spring", stiffness: 260, damping: 20 },
-  },
-};
+import { buttonVariants } from "@/utils/motion/motions.utilities";
 
 export default function LandingButton() {
   const router = useRouter();
@@ -19,9 +11,9 @@ export default function LandingButton() {
   return (
     <motion.button
       onClick={handleClick}
-      variants={buttonVariants}
       initial="hidden"
       animate="show"
+      variants={buttonVariants}
       className="bg-pc-violet hover:bg-pc-violet-100 font-pc-lato font-bold uppercase text-sm flex items-center gap-2 p-4 rounded-2xl cursor-pointer transition-colors duration-300 ease-in-out"
     >
       Probar chatbot <MdOutlineArrowOutward />
