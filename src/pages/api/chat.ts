73edbbe8eb68301dab40f70bdbc82738cab0bc5c
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-07-03 17:28:45 -0300

    update chat module

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios, { AxiosError } from "axios";
 
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/chat`;
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -11,10 +13,7 @@ export default async function handler(
   }
 
   try {
-    const response = await axios.post(
-      "http://18.118.162.255:8000/chat",
-      req.body
-    );
+    const response = await axios.post(API_URL, req.body);
     return res.status(response.status).json(response.data);
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
